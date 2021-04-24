25fae16bcddb422182d18d44bd50c93a70131943
Daniel B <dantheman41@live.com.au>
2021-04-24 15:27:33 +1000

    Optimise grid to be based on changes, not full refreshes

--- Sources/Grid.hx
+++ Sources/Grid.hx
@@ -12,20 +12,19 @@ class Grid {
     var tiles:Array<Int> = [];
     var bodies:Array<Body> = [];
     var space:Space;
-    var dirty = false;
 
     public function new(space) {
         for (x in 0...width) {
             for (y in 0...height) {
                 tiles.push(1);
+                bodies.push(null);
             }
         }
         this.space = space;
+
         constructShapes();
     }
     public function update() {
-        if (dirty)
-            constructShapes();
     }
     public function render(g:Graphics){
         for (x in 0...width) {
@@ -36,33 +35,57 @@ class Grid {
             }
         }
     }
+    function makeBody(x, y) {
+        var body = new Body(BodyType.STATIC);
+        body.shapes.add(new Polygon(Polygon.rect(x*20, 600+y*20, 20, 20)));
+        body.space = space;
+
+        bodies[x*height+y] = body;
+    }
     public function remove(x,y) {
         if (x < 0 || y < 0 || x >= width || y >= height)
             return;
         tiles[x*height+y] = 0;
-        dirty = true;
+
+        if (bodies[x*height+y] != null) {
+            bodies[x*height+y].space = null;
+            bodies[x*height+y] = null;
+        }
+
+        if (x > 0 && unsafeGetTile(x-1,y) == 1 && getBody(x-1,y) == null) {
+            makeBody(x-1,y);
+        }
+        if (x < width-1 && unsafeGetTile(x+1,y) == 1 && getBody(x+1,y) == null) {
+            makeBody(x+1,y);
+        }
+        if (y > 0 && unsafeGetTile(x,y-1) == 1 && getBody(x,y-1) == null) {
+            makeBody(x,y-1);
+        }
+        if (y < height-1 && unsafeGetTile(x,y+1) == 1 && getBody(x,y+1) == null) {
+            makeBody(x,y+1);
+        }
+    }
+    public inline function unsafeGetTile(x,y) {
+        return tiles[x*height+y];
     }
     public function getTile(x,y) {
         if (x < 0 || y < 0 || x >= width || y >= height)
             return 0;
         return tiles[x*height+y];
     }
-    public function constructShapes() {
-        for (body in bodies) {
-            body.space = null;
-        }
-        bodies = [];
+    public inline function getBody(x,y) {
+        // if (x < 0 || y < 0 || x >= width || y >= height)
+        //     return null;
+        return bodies[x*height+y];
+    }
 
+    public function constructShapes() {
         for (x in 0...width) {
             for (y in 0...height) {
                 if (getTile(x,y) == 1 && (getTile(x-1,y) == 0 || getTile(x+1,y) == 0 || getTile(x,y-1) == 0 || getTile(x,y+1) == 0)) {
-                    var body = new Body(BodyType.STATIC);
-                    body.shapes.add(new Polygon(Polygon.rect(x*20, 600+y*20, 20, 20)));
-                    body.space = space;
-                    bodies.push(body);
+                    makeBody(x,y);
                 }
             }
         }
-        // body.space = space;
     }
 }
